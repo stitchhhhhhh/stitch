9e3ff50b3205338d927899d650142cd8e4981c5f
bunga ayu <bunga.maysyafa@student.president.ac.id>
2026-06-24 16:33:24 +0700

    feat: initial backend setup

---
+++ prisma.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import { defineConfig } from "prisma/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+  migrate: {
+    adapter: () => {
+      const pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+      });
+      return new PrismaPg(pool);
+    },
+  },
+});
